7b875bce13e72c9c37a9ffe858c1ba4b911e3244
Tobias Leinss <tobias.leinss@mway.io>
2024-05-20 14:19:17 +0200

    Add filter agent task

--- farcaster_backend/agent_crew.py
+++ farcaster_backend/agent_crew.py
@@ -1,5 +1,5 @@
 import os
-from crewai import Agent, Crew, Process
+from crewai import Agent, Crew, Process, Task
 from langchain_groq import ChatGroq
 import dotenv
 
@@ -25,9 +25,21 @@ cleanerAgent = Agent(
     memory=True,
 )
 
+filterTask = Task(
+    description="""
+            Receive a long list of posts from the user's fid {fid} feed for the last 24 hours. Process each post, determine its relevance to the user's specified topic of interest: \n {interests} \n, and filter out any posts that do not match this interest. 
+            24H feed:\n\n {casts} \n\n
+            Context: The feed is from a decentralized social media network/protcool called, farcaster. Warpcast is the first and biggest client on this protocol. The network is built on Optimism (Layer2 on Ethereum) and is similar to twitter and reddit. Users can post called 'cast', own their identity and data and can freely move to another client app, without losing their network and data. Channels on farcaster written as /channel_name are similar to subreddits or groups in SM platforms. Most popular activity on farcaster is tipping memecoins, most famously $DEGEN. Other famous memecoins are $TN100x aka Ham, floties, $onchain, $higher, $build (where builders nominate (tip) each other), $enjoy and bunch of other. Most these memecoins are on Base network (A layer2 on ethereum). Base is the most used blockchain network around where most activities happen on farcaster. 
+            """,
+    expected_output="""
+            A list of filtered social media posts in JSON format. No preamble or epilogue, only pure parsable JSON.
+            """,
+    agent=cleanerAgent,
+)
+
 AgentCrew = Crew(
     agents=[cleanerAgent],
-    tasks=[],
+    tasks=[filterTask],
     verbose=2,
     process=Process.sequential,
 )
@@ -40,7 +52,8 @@ casts = [
     {"text": "text4"},
     {"text": "text5"},
 ]
+interests = ["AI", "web3"]
 
-result = AgentCrew.kickoff(inputs={"fid": fid, "casts": casts})
+result = AgentCrew.kickoff(inputs={"fid": fid, "casts": casts, "interests": interests})
 
 print(f"result {result}\n")
