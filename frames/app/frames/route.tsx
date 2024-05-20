1b7e0d7abd30e5df25b16706fcdb6f9c4ed9ed94
Tobias Leinss <tobias.leinss@mway.io>
2024-05-20 13:19:39 +0200

    Work on backend frames logic, put placeholder

--- frames/app/frames/route.tsx
+++ frames/app/frames/route.tsx
@@ -11,70 +11,36 @@ const images = [
 
 const frameHandler = frames(async (ctx: any) => {
   const page = Number(ctx.searchParams?.pageIndex ?? 0)
-
-  const counter = ctx.message
-    ? ctx.searchParams.op === "+"
-      ? ctx.state.counter + 1
-      : ctx.state.counter - 1
-    : ctx.state.counter
+  const op = ctx.searchParams?.op
+  const inputText = ctx.message?.inputText
+  const requesterFid = ctx.message?.requesterFid
 
   if (page === 0) {
     return {
       image: images[page]!,
-      // (
-      //   <div tw="flex flex-col">
-      //     <div tw="flex">Milk your Feed</div>
-
-      //     <div tw="flex">Cream your Interest</div>
-
-      //     {/* {ctx.message?.inputText && (
-      //     <div tw="flex">{`Input: ${ctx.message.inputText}`}</div>
-      //   )} */}
-      //     {/* <div tw="flex">Test {counter}</div> */}
-      //   </div>
-      // ),
       imageOptions: {
         aspectRatio: "1:1",
       },
-      // textInput: "Say something",
       buttons: [
         <Button
           action="post"
-          // target={{ pathname: "/", query: { op: "+" } }}
           target={{
             query: {
-              pageIndex: String(page + 1), // % nfts.length),
+              pageIndex: String(page + 1),
             },
           }}
         >
           Start
         </Button>,
-        // <Button action="post" target={{ pathname: "/", query: { op: "-" } }}>
-        //   Decrement
-        // </Button>,
-        // <Button action="link" target={appURL()}>
-        //   External
-        // </Button>,
       ],
-      state: { counter: counter },
     }
   }
 
   if (page === 1) {
+    console.log(`fetch here feed of fid ${requesterFid}`)
+
     return {
-      image: (
-        <div tw="flex flex-col">
-          <div tw="flex">
-            Type in your interests to customize your 24h feed digest
-          </div>
-
-          <div tw="flex">or let us choose for you!</div>
-
-          {ctx.message?.inputText && (
-            <div tw="flex">{`Input: ${ctx.message.inputText}`}</div>
-          )}
-        </div>
-      ),
+      image: images[page]!,
       imageOptions: {
         aspectRatio: "1:1",
       },
@@ -104,61 +70,44 @@ const frameHandler = frames(async (ctx: any) => {
         >
           Typed my interest!
         </Button>,
-        // <Button action="link" target={appURL()}>
-        //   External
-        // </Button>,
       ],
-      state: { counter: counter },
     }
   }
 
   if (page === 2) {
-    return {
-      image: (
-        <div tw="flex flex-col">
-          <div tw="flex">
-            AI Agents crew is tuning into your interests and brewing your
-            Castlatte!
-          </div>
+    console.log(
+      `let the agents run on ${
+        op === "choose_interest_for_me"
+          ? "chooese for me"
+          : `typed my interest ${ctx.message?.inputText}`
+      }`
+    )
 
-          <div tw="flex">Try in 2 minutes!</div>
-        </div>
-      ),
+    return {
+      image: images[page]!,
       imageOptions: {
         aspectRatio: "1:1",
       },
-      textInput: "Type in your interest here...",
       buttons: [
         <Button
           action="post"
           target={{
             pathname: "/",
             query: {
-              pageIndex: String(page + 1), // % nfts.length),
-              op: "choose_interest_for_me",
-            },
-          }}
-        >
-          Choose for me!
-        </Button>,
-        <Button
-          action="post"
-          target={{
-            pathname: "/",
-            query: {
               pageIndex: String(page + 1),
-              op: "typed_interest",
+              op: "serve_castlatte",
             },
           }}
         >
-          Typed my interest!
+          Serve my Castlatte
         </Button>,
       ],
-      state: { counter: counter },
     }
   }
 
   if (page === 3) {
+    console.log("check if agents have finished, if not show page 2 again")
+
     return {
       image: (
         <div tw="flex flex-col">
@@ -167,34 +116,7 @@ const frameHandler = frames(async (ctx: any) => {
           <div tw="flex">AI generated summary</div>
         </div>
       ),
-      textInput: "Type in your interest here...",
-      buttons: [
-        <Button
-          action="post"
-          target={{
-            pathname: "/",
-            query: {
-              pageIndex: String(page + 1), // % nfts.length),
-              op: "choose_interest_for_me",
-            },
-          }}
-        >
-          Choose for me!
-        </Button>,
-        <Button
-          action="post"
-          target={{
-            pathname: "/",
-            query: {
-              pageIndex: String(page + 1),
-              op: "typed_interest",
-            },
-          }}
-        >
-          Typed my interest!
-        </Button>,
-      ],
-      state: { counter: counter },
+      buttons: [],
     }
   }
 
